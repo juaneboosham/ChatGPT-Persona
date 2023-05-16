22c191451f650ed70f899e7dc168b6ce40114e2c
longzhan.huang <longzhan.huang@thoughtworks.com>
2023-05-16 17:12:40 +0800

    chore: modify name

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -93,7 +93,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT Peasona</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
